7cf7d6a544f0219cf74279c5e4e05001cb8f4d38
abusaidshabib <abusaidshabib712@gmail.com>
2024-09-11 00:07:01 +0600

    menu done

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, AboutView, BlogView, ContactView, FAQView, ServiceView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='main_home')
+    path('', HomeView.as_view(), name='main_home'),
+    path('about/', AboutView.as_view(), name='main_about'),
+    path('blog/', BlogView.as_view(), name='main_blog'),
+    path('contact/', ContactView.as_view(), name='main_contact'),
+    path('faq/',FAQView.as_view(), name='main_faq'),
+    path('service/', ServiceView.as_view(), name='main_services')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -5,4 +5,24 @@ from django.shortcuts import render, redirect
 # Create your views here.
 class HomeView(View):
     def get(self, request):
-        return render(request, 'mainbase.html')
+        return render(request, 'pages/home.html')
+
+class AboutView(View):
+    def get(self, request):
+        return render(request, 'pages/about.html')
+
+class BlogView(View):
+    def get(self, request):
+        return render(request, 'pages/blog.html')
+
+class ContactView(View):
+    def get(self, request):
+        return render(request, 'pages/contact.html')
+
+class FAQView(View):
+    def get(self, request):
+        return render(request, 'pages/faq.html')
+
+class ServiceView(View):
+    def get(self, request):
+        return render(request, 'pages/services.html')
