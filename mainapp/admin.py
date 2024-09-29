d8a5e97b74ee65a150735dc605b3748db5130b7b
mdmarjanhossain <mdmarjanhossain0@gmail.com>
2024-09-30 00:31:19 +0600

    update

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -5,15 +5,27 @@ from .models import Service, BlogPost, Ticket, Message, ServiceBooking
 
 @admin.register(Service)
 class UserServiceAdmin(admin.ModelAdmin):
-    list = ('id', 'title')
+    list = ('pk', 'title', 'price', 'created')
 
 @admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):
-    list = ['id', 'title']
+    list_display = ['pk', 'title', 'created_by', 'is_accepted', 'published_at']
+    search_fields = ["title", "created_at__username"]
+    list_editable = ["is_accepted",]
+    list_filter = ["created_by", "is_accepted"]
 
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ['subject', 'user', 'staff', 'created_at', 'updated_at']
 
-admin.site.register(Ticket)
-admin.site.register(Message)
-admin.site.register(ServiceBooking)
+@admin.register(Message)
+class TicketMessageAdmin(admin.ModelAdmin):
+    list_display = ['user', 'content', 'created_at', 'updated_at']
+
+    
+@admin.register(ServiceBooking)
+class ServiceBookingAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'user', 'booking_date', 'status']
+
 
 admin.site.site_header = 'Aaplify dashboard'
